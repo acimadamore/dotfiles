96cb1296974ddad944f60decce81f37d92e0777f
Andrés Cimadamore <andrescimadamore@gmail.com>
2022-08-08 02:47:34 -0300

    Remove setup script. Just store dotfiles

--- setup/bat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bat::configure() {
-  if $(! command_exists "bat"); then
-    mkdir -p ~/.local/bin
-    ln -s /usr/bin/batcat ~/.local/bin/bat
-  fi
-}
--- setup/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker::configure() {
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-
-  notice "Docker configured"
-}
